8b34788943bb206091cfef102ed3357e544b6162
Lilliana Ramos <lilliana.cramos@gmail.com>
2019-12-12 16:30:18 -0500

    styling page

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
     // 
     const weatherUrl = "http://api.openweathermap.org/data/2.5/weather?q="
-    const weatherApiKey = "&appid=8526bab28d6f75f024123eb744a72998"
+    const weatherApiKey = "appid=8526bab28d6f75f024123eb744a72998"
+    
 
 function currentWeather(searchValue){
     $.ajax({
